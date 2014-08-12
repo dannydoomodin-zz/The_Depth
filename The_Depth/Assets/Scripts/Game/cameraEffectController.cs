e87873b6b828ce65a76e8cea57494351e3c84a5a
Wei Danny <Wei@DannyWin8.(none)>
2014-08-12 23:06:54 +1200

    added camera effect and shader for time device functionality

---
+++ The_Depth/Assets/Scripts/Game/cameraEffectController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class cameraEffectController : MonoBehaviour {
+
+	private bool isUsingTDevice = false;
+
+	public Shader shader;
+	public void Awake() 
+	{
+		if (shader)
+		{
+			transform.camera.SetReplacementShader(shader, null);
+		}
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			if(isUsingTDevice)
+			{
+				isUsingTDevice = false;
+				transform.camera.SetReplacementShader(shader, null);
+			}
+			else
+			{
+				isUsingTDevice = true;
+				transform.camera.SetReplacementShader(null, null);
+			}
+		}
+	}
+}
