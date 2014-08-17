d15f4dc286c184661fa564fc607b1b1f8bf1892f
Wei Danny <Wei@DannyWin8.(none)>
2014-08-17 21:35:29 +1200

    added time device and code for animation in diving room

---
+++ The_Depth/Assets/Scripts/Game/DivingRoom.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class DivingRoom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Util.clickedUI)
+		{
+			Util.clickedUI = false;
+			return;
+		}
+			
+		if(Input.GetMouseButtonUp(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+				
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				Debug.Log(hit.transform.gameObject.name);
+
+				if(hit.transform.gameObject.name.Contains("Locker"))
+				{
+					Animation anim = hit.transform.gameObject.animation;
+					if(!anim)
+					{
+						anim = hit.transform.parent.animation;
+					}
+
+					if(anim)
+					{
+						anim.Play ("Open");
+					}
+				}
+
+			}
+		}
+	}
+}
