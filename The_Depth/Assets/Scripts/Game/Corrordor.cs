b5b515b1f682f5bf9d630ef2c42d6910b9bdbd23
Wei Danny <Wei@DannyWin8.(none)>
2014-09-18 11:46:40 +0930

    added library door plus clicked interaction

---
+++ The_Depth/Assets/Scripts/Game/Corrordor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corrordor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Util.clickedUI)
+		{
+			Util.clickedUI = false;
+			return;
+		}
+		
+		if(Input.GetMouseButtonUp(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				Debug.Log(hit.transform.gameObject.name);
+
+				if (hit.transform.gameObject.name == "fancyDoor")
+				{
+					Util.setDialogue("It's locked...seems to be locked from the inside...");
+				}
+
+			}
+		}
+	}
+}
